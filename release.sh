2ce4f51d5ca14a8c9aa79975e777bc676bdd113f
rebootless <sbox.simple@protonmail.com>
2026-07-20 16:52:43 +0300

    Update release script to copy assets and world
    Remove symbolic links and copy assets and world directories.

--- release.sh
+++ release.sh
@@ -15,13 +15,11 @@ if [[ ! -f "dungeon" ]]; then
     exit 1
 fi
 
-# CMake POST_BUILD оставил assets/world как симлинки — заменяем их реальными копиями
 rm -f assets world
 
 cp -r ../assets .
 cp -r ../world .
 
-# Убираем мусор cmake, оставляя только релизные файлы
 rm -rf CMakeFiles CMakeCache.txt cmake_install.cmake Makefile
 
 echo "SUCCESS"
